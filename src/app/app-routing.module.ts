6d95e1ba7dce76d32cd258d0f0ef0fcdf7d9a943
Yohan0412 <Yohan.rosano@hotmail.fr>
2025-01-16 05:06:47 +0100

    Mise en place du router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { RouterLink } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+  const routes: Routes = [
+
+  {path:'', component: HomeComponent },
+  {path:'admin', component: AdminComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterLink , RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
